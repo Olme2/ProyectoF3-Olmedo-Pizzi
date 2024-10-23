6fec766fcb835d781363fc13adb30856083b0360
Olme2 <165046979+Olme2@users.noreply.github.com>
2024-10-23 11:58:29 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,90 @@
+<?php
+    $ruta='';
+    $ruta2='';
+    $ruta3='php/';
+    include 'php/encabezado.php';
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <title>Proyecto Física III</title>
+</head>
+
+<body>
+    <header class="container-fluid bg-primary text-white text-center p-3">
+        <h1>Nuestro proyecto Física III</h1>
+    </header>
+    <main class="container-fluid p-4">
+        <div class="row">
+            <section class="col-md-8">
+                <h2>Bienvenidos a nuestro proyecto de promoción</h2>
+                <p>
+                    Nuestro proyecto de promoción consta del desarrollo de una útil página para los alumnos en donde se pueda visualizar información útil y realizar diferentes tipos de cálculos correspondientes al tema visto en el cursado de la materia Física III: Óptica geométrica.
+                </p>
+                <h3>Integrantes</h3>
+                <div class="row">
+                    <div class="col-md-6">
+                        <img src="img/Imagen de WhatsApp 2023-12-14 a las 15.03.09_a0777560.jpg" alt="Valentino Olmedo" title="Valentino Olmedo" class="img-fluid">
+                        <p>Valentino Olmedo</p>
+                    </div>
+                    <div class="col-md-6">
+                        <img src="img/Imagen de WhatsApp 2023-12-14 a las 15.18.45_34770566.jpg" alt="Franco Pizzi" title="Franco Pizzi" class="img-fluid">
+                        <p>Franco Pizzi</p>
+                    </div>
+                </div>
+                <p>Somos Valentino Olmedo y Franco Pizzi, alumnos que cursaron la asignatura de Física III en el primer cuatrimestre del año 2023 en la Facultad de Ciencias Exactas y Tecnología de la UNT.</p>
+            </section>
+            <aside class="col-md-4">
+                <div class="row mb-3">
+                    <div class="col-md-12">
+                        <img src="img/Imagen de WhatsApp 2023-12-14 a las 15.01.31_dcba45cb.jpg" alt="Imagen Personalizada" class="img-fluid">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="img/Logo-AH-01.jpg" class="img-fluid">
+                    </div>
+                </div>
+            </aside>
+        </div>
+    </main>
+
+    <?php
+    include 'php/footer.php';
+    ?>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
